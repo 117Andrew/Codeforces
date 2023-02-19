4b8b8b3c8de38a55b593120ccc5c7ffa9eb36b5c
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-19 19:54:21 -0300

    gitignore evita archivos de comp y primer submit 231A

--- 231A/julian.cpp
+++ 231A/julian.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 int main()
 {
-    int n, a, b, c;
-    queue <int> q;
+    int n, a, b, c, x, z=0;
     cin >> n;
+
     for (int i=0;i<n;i++)
     {
+        x = 0;
         cin >> a >> b >>c;
-        q.push(a);
-        q.push(b);
-        q.push(c);
+        x = (a+b+c);
+
+        if (x>=2)
+        {
+            z++;
+        }
     }
-    for (int i=0;i<n;i++)
-    {
-        
-    }    
+
+    cout << z << "\n";
     return 0;
 }
