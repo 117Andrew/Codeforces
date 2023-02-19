75cda4813bbb33645509e4ca1fbe180018d1733e
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-19 16:47:04 -0300

    primer for de ingreso de datos listo

---
+++ 231A/julian.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    int n, a, b, c;
+    queue <int> q;
+    cin >> n;
+    for (int i=0;i<n;i++)
+    {
+        cin >> a >> b >>c;
+        q.push(a);
+        q.push(b);
+        q.push(c);
+    }
+    for (int i=0;i<n;i++)
+    {
+        
+    }    
+    return 0;
+}
