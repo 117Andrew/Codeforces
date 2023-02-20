bef8a0a078867434fed0c28f6f0971901ddd8be8
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-20 00:52:29 -0300

    input y readme de 158A hechos

---
+++ 158A/julian.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int p, n, a;
+    queue <int> q;
+    cin >> p;
+    for (int i=0;i<p;i++)
+    {
+        cin >> n;
+        q.push(n);
+    }
+    
+    return 0;
+}
