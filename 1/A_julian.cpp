4635b6d2349bba525fd99d2f9d15cfc8dcaee9ca
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-02-20 20:41:41 -0300

    Segundo submit

--- 1/A_julian.cpp
+++ 1/A_julian.cpp
@@ -3,20 +3,13 @@ using namespace std;
 
 int main()
 {
-int m, n, a, x=0;
+int m, n, a, l, x=0;
     cin >> m >> n >> a;
-    while ((m>0) || (n>0))
+    l= m+n;
+    while (l>0)
     {
-        if (m>0)
-        {
-            m-=a;
-            x++;
-        }
-        if (n>0)
-        {
-            n-=a;
-            x++;
-        }
+        l-=a;
+        x++;
     }
     cout << x << "\n";
     return 0;
