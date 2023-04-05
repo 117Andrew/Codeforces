4f563f42ea2ea10cf8a4cb72e877ce7eb6e8cc0d
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-04-04 22:49:09 -0300

    actualizacion del problema A

--- 1811/A_julian.cpp
+++ 1811/A_julian.cpp
@@ -7,9 +7,13 @@ int main()
     int t=0,n=0;
     string d, s;
     cin >> t;
-    for (int i=0; i<t; i++){
+    for (int i=0; i<t; i++)
+    {
         cin >> n >> d >> s;
-        cout << s;
+        for (int j=0;j<n;j++)
+        {
+
+        }
     }
     return 0;
 }
