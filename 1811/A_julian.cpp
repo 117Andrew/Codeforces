264f68ec36ce8035a557088122ec9ced53d12126
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-04-08 23:21:20 -0300

    Añadido el handbook y submit correcto 1811/A

--- 1811/A_julian.cpp
+++ 1811/A_julian.cpp
@@ -4,21 +4,63 @@ using namespace std;
 
 int main()
 {
-    int t=0,n=0;
-    char d, x;
-    string s,sf;
+    int t,n,b;
+    char d, x, aux;
+    string s, a="0", dd;
     cin >> t;
     for (int i=0; i<t; i++)
     {
         cin >> n >> d >> s;
+        b=0;
+        dd=d;
         for (int j=0;j<n;j++)
         {
-            if (s[j]<d)
+            if (d=='0')
             {
-                x=s[j];
-                s[j]=d;
+                s=s.append(dd);
+                b=1;
+                break;
+            }
+            else if (d=='9')
+            {
+                s=dd.append(s);
+                b=1;
+                break;
+            }
+            else if (s[j] < d)
+            {
+                if (j==n-1)
+                {
+                    s.append(a);
+                    x = s[j];
+                    s[j]=d;
+                    s[j+1]=x;
+                    b=1;
+                    break;
+                }
+                else
+                {
+                    s.append(a);
+                    x=s[j];
+                    s[j]=d;
+                    b=1;
+                    for (int k=j+1;k<n+1;k++)
+                    {
+                        aux=s[k];
+                        s[k]=x;
+                        x=aux;
+                    }
+                    break;
+                }
             }
         }
+        if (b==0)
+        {
+            s.append(a);
+            s[n]=d;
+        }
+
+        cout << s << '\n';
     }
     return 0;
 }
--- 1811/readme.md
+++ 1811/readme.md
@@ -1,3 +1,4 @@
 ##link del contest:
 	https://codeforces.com/contest/1811
-
+##Log:
+Codigo funcionando, sin optimizacion pertinente y realizado por manipulacion de strings.
