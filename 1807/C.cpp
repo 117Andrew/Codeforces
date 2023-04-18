1925f95b7b3847b0fd47f4244b90eb9009c8a805
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-04-17 23:53:12 -0300

    Primer intento 1807/C

---
+++ 1807/C.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t, n, b, cont;
+    string s;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        b = 0, cont = 0;
+        cin >> n >> s;
+        for (int j = 0; j < n && b == 0; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                if (s[j] != s[k])
+                    cont++;
+                else
+                {
+                    if (cont%2 == 0)
+                        cont = 0;
+                    else
+                    {
+                        b = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        if (b == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
