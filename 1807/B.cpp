82c4706aa4e75622df6df9ac81f58f2550157bb3
Julian A. Albanesi <andresalbanesi@gmail.com>
2023-04-17 23:06:40 -0300

    Mod gitignore y submit correcto 1807/B

--- 1807/B.cpp
+++ 1807/B.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int t, l, n;
-vector <int> c;
 
 int main()
 {
+int t, l, n, m, b;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
+        m = 0, b = 0;
         cin >> l;
         for (int j = 0; j < l; j++)
         {
             cin >> n;
-            c.push_back(n);
+            if (n%2 == 0)
+            {
+                m += n;
+            }
+            else
+            {
+                b += n;
+            }
         }
+        if (m > b)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+
         
     }
+    return 0;
 }
